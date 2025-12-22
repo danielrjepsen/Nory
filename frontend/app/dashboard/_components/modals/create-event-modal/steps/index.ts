94adcd0e38b4c961a4397aaca8f95a7ead710be7
Daniel Jepsen <73501043+danielrjepsen@users.noreply.github.com>
2025-12-23 00:53:53 +0100

    frontend: refactor create event modal

--- frontend/app/dashboard/_components/modals/create-event-modal/steps/index.ts
+++
@@ -1,6 +0,0 @@
-export { EventTypeStep } from './EventTypeStep';
-export { BasicInfoStep } from './BasicInfoStep';
-export { ScheduleStep } from './ScheduleStep';
-export { SettingsStep } from './SettingsStep';
-export { GuestAppStep } from './GuestAppStep';
-export { ReviewStep } from './ReviewStep';
