13fd4a8eed114b12304ac4baee64a7e511998861
Daniel Jepsen <73501043+danielrjepsen@users.noreply.github.com>
2026-01-06 11:19:20 +0100

    frontend: language setup

--- frontend/lib/i18n/config.ts
+++ frontend/lib/i18n/config.ts
@@ -2,17 +2,24 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import da from './locales/da';
+import en from './locales/en';
 
 export const defaultNS = 'common';
 export const defaultLocale = 'da';
-export const supportedLocales = ['da'] as const;
+export const supportedLocales = ['da', 'en'] as const;
 export type SupportedLocale = (typeof supportedLocales)[number];
+
+export const localeLabels: Record<SupportedLocale, string> = {
+  da: 'Dansk',
+  en: 'English',
+};
 
 export const namespaces = ['common', 'auth', 'dashboard', 'slideshow', 'remote', 'wizard'] as const;
 export type Namespace = (typeof namespaces)[number];
 
 export const resources = {
   da,
+  en,
 } as const;
 
 i18n.use(initReactI18next).init({
